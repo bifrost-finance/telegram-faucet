71175e3b274ed066e35d85651fa7ed013890396a
lurpis <hi@lurpis.com>
2020-08-02 22:12:53 +0800

    update parameter

--- src/MatchTelegram.js
+++ src/MatchTelegram.js
@@ -129,7 +129,7 @@ class MatchTelegram {
                   message += targetAddress + ' has received: \n';
                   message += amount.asg + ' ASG      ' + amount.ausd + ' aUSD\n';
                   message += amount.dot + ' DOT      ' + amount.ksm + ' KSM\n\n';
-                  message += 'Use them in https://dash.bifrost.finance for test (OWNS NO VALUE)';
+                  message += 'Explorer: https://bifrost.subscan.io\nUse them in https://dash.bifrost.finance for test (OWNS NO VALUE)';
 
                   await bot.sendMessage(msg.chat.id, message);
 
